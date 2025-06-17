c45aaf5101070aff534ea9f7215b9d8f3fb36de0
Joey Escareno <joe_escareno@hotmail.com>
2025-06-17 15:47:08 -0500

    Fix cross-compilation toolchain and update build scripts

--- build/CMakeFiles/3.22.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -3,13 +3,13 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.6.87.1-microsoft-standard-WSL2")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
+include("/app/jetson_orin.cmake")
 
-
-set(CMAKE_SYSTEM "Linux-6.6.87.1-microsoft-standard-WSL2")
+set(CMAKE_SYSTEM "Linux")
 set(CMAKE_SYSTEM_NAME "Linux")
-set(CMAKE_SYSTEM_VERSION "6.6.87.1-microsoft-standard-WSL2")
-set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
-set(CMAKE_CROSSCOMPILING "FALSE")
+set(CMAKE_CROSSCOMPILING "TRUE")
 
 set(CMAKE_SYSTEM_LOADED 1)
